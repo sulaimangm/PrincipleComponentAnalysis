527006a292e9485cfacc9bec154a0135c7a90f48
sulaimangm <42448524+sulaimangm@users.noreply.github.com>
2022-01-22 01:29:21 -0500

    Update PCA.py

--- PCA.py
+++ PCA.py
@@ -1,4 +1,7 @@
-#Here we will perform Principal Component Analysis on a self generated random Data Set We will use SKLearn's in-built Library for LDA for performing classification
+'''We will generate a random covariance matrix for each of our 2 classes. With this matrix we will generate a dataset for each of the class, with each class having 10
+attributes and 1000 rows. We then combine the data and split it into Train and Test sets. We then calculate the Eigen values and Eigen vectors of the data. 
+Then multiply it with the training and testing data and then calculate the classification error. We then try to reconstruct the data and calculate the Mean Square Error
+and plot both of them in a graph.'''
 
 #Importing Libraries
 import numpy as np
